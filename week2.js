74ed495f452eb5cd55b746604b5e0eca55fd2d18
hazim <hazimfahmi@gmail.com>
2023-03-30 15:54:14 +0800

    week 2 thursday

---
+++ week2.js
@@ -0,0 +1,36 @@
+//let message = "benr"
+//console.log(message, typeof message)
+//message = 180
+//console.log(message, typeof message)
+
+
+/*let str = "BENR"
+let num = 3523
+let course = str + num
+console.log(course, typeof course)*/
+
+/*let str = '1'
+let num = 1
+let total = str + num 
+let total2 = parseInt(str) + num
+console.log(total,typeof total)
+console.log(total2,typeof total2)*/
+
+/*let array = ["benr",163849,false]
+console.log(array[0])
+console.log(array[1])
+console.log(array[2])
+
+
+let data = {name:"kapurekeke", age:"22", faculty:"fkekk"}
+console.log(data.name)
+console.log(data.age)
+console.log(data.faculty)*/
+
+
+let message =   [{name:"user1",date:"30/3/2023",comment:"good vid",like:1020}
+,{name:"user2",date:"1/4/2023",comment:"very good vid",like:90}]
+
+console.log(message[1].name)
+console.log(message[1].comment)
+console.log(message[1].like)
